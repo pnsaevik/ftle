742517e130c057f9dce47bf781d4bd29ba0bca51
Pål Næverlid Sævik <paal.naeverlid.saevik@hi.no>
2022-09-26 12:53:07 +0200

    Add stubs

--- src/ftle/ladim2ftle.py
+++ src/ftle/ladim2ftle.py
@@ -1,2 +1,22 @@
-def main(infile, outfile):
+import xarray as xr
+
+
+def append(dset, outfile):
     pass
+
+
+def run(ladim_dset):
+    return xr.Dataset()
+
+
+def main(infile, outfile):
+    dset_in = xr.load_dataset(infile)
+    dset_out = run(dset_in)
+    append(dset_out, outfile)
+
+
+if __name__ == '__main__':
+    main(
+        infile=r"C:\Users\a5606\Downloads\lyapunov\NK800_20161221_005m.nc",
+        outfile=r"C:\Users\a5606\Downloads\lyapunov\output.nc",
+    )
