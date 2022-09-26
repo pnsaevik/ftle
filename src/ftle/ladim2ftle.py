4c89adf12160cdd7d6a36d940de26c0f59830dc2
Pål Næverlid Sævik <paal.naeverlid.saevik@hi.no>
2022-09-26 12:45:59 +0200

    Add ladim2ftle script

---
+++ src/ftle/ladim2ftle.py
@@ -0,0 +1,2 @@
+def main(infile, outfile):
+    pass
--- src/ftle/script.py
+++ src/ftle/script.py
@@ -1,2 +1,9 @@
+def ladim2ftle():
+    import sys
+    args = sys.argv[1:]
+    from . import ladim2ftle
+    ladim2ftle.main(infile=args[0], outfile=args[1])
+
+
 def main():
     pass
