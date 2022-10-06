783e6621f63ce9be7ea405f89c0e19445b9526cb
Pål Næverlid Sævik <paal.naeverlid.saevik@hi.no>
2022-10-06 13:53:41 +0200

    Add higher-order advection

--- src/ftle/tracker.py
+++ src/ftle/tracker.py
@@ -1,8 +1,35 @@
-def advection(x, y, u, v, dt, order=1):
+import numpy as np
+
+
+def advect(x, y, z, t, u, v, w, dt, order):
+    def velocity(crd_in, t_in):
+        return np.stack([u(*crd_in, t_in), v(*crd_in, t_in), w(*crd_in, t_in)])
+
+    c0 = np.stack([x, y, z])
+
     if order == 1:
-        x2 = x + u.interp(x=x, y=y) * dt
-        y2 = y + v.interp(x=x, y=y) * dt
+        c_final = c0 + velocity(c0, t) * dt
+
+    elif order == 2:
+        t1 = t + 0.5 * dt
+        c1 = c0 + 0.5 * dt * velocity(c0, t)
+        c_final = c0 + dt * velocity(c1, t1)
+
+    elif order == 4:
+        v1 = velocity(c0, t)
+        c1 = c0 + 0.5 * dt * v1
+
+        v2 = velocity(c1, t + 0.5 * dt)
+        c2 = c0 + 0.5 * dt * v2
+
+        v3 = velocity(c2, t + 0.5 * dt)
+        c3 = c0 + v3 * dt
+
+        v4 = velocity(c3, t + dt)
+
+        c_final = c0 + (dt / 6) * (v1 + 2 * v2 + 2 * v3 + v4)
+
     else:
         raise NotImplementedError(f'Unknown integration order: {order}')
 
-    return x2, y2
+    return c_final[0], c_final[1], c_final[2], t + dt
--- tests/test_tracker.py
+++ tests/test_tracker.py
@@ -1,15 +1,52 @@
 from ftle import tracker
-import xarray as xr
 import numpy as np
+import pytest
 
 
-class Test_advection:
-    def test_correct_when_homogeneous_field_and_euler(self):
-        x = xr.Variable('pid', [1., 2., 3.])
-        y = xr.Variable('pid', [1., 2., 3.])
-        u = xr.DataArray(dims=('y', 'x'), data=2*np.ones((5, 6)))
-        v = xr.DataArray(dims=('y', 'x'), data=np.ones((5, 6)))
+class Test_advect:
+    @pytest.fixture(scope='class')
+    def params(self):
+        def u(xx, *_):
+            return np.ones_like(xx)
+
+        def v(xx, *_):
+            return 2*np.ones_like(xx)
+
+        def w(xx, *_):
+            return 3*np.ones_like(xx)
+
+        x = np.array([1, 2, 3, 4], dtype='f4')
+        y = np.array([4, 5, 6, 7], dtype='f4')
+        z = np.array([7, 8, 9, 10], dtype='f4')
+        t = 10
         dt = 1
-        x2, y2 = tracker.advection(x, y, u, v, dt)
-        assert x2.values.tolist() == [3, 4, 5]
-        assert y2.values.tolist() == [2, 3, 4]
+
+        return x, y, z, t, u, v, w, dt
+
+    def test_correct_when_linear_field_and_order_1(self, params):
+        x, y, z, t, u, v, w, dt = params
+        x2, y2, z2, t2 = tracker.advect(x, y, z, t, u, v, w, dt, order=1)
+
+        assert x2.tolist() == [2, 3, 4, 5]
+        assert y2.tolist() == [6, 7, 8, 9]
+        assert z2.tolist() == [10, 11, 12, 13]
+        assert t2 == t + dt
+
+    def test_correct_when_linear_field_and_order_2(self, params):
+        x, y, z, t, u, v, w, dt = params
+        x2, y2, z2, t2 = tracker.advect(x, y, z, t, u, v, w, dt, order=2)
+
+        assert x2.tolist() == [2, 3, 4, 5]
+        assert y2.tolist() == [6, 7, 8, 9]
+        assert z2.tolist() == [10, 11, 12, 13]
+        assert t2 == t + dt
+
+    def test_correct_when_linear_field_and_order_4(self, params):
+        x, y, z, t, u, v, w, dt = params
+        x2, y2, z2, t2 = tracker.advect(x, y, z, t, u, v, w, dt, order=4)
+
+        assert x2.tolist() == [2, 3, 4, 5]
+        assert y2.tolist() == [6, 7, 8, 9]
+        assert z2.tolist() == [10, 11, 12, 13]
+        assert t2 == t + dt
+
